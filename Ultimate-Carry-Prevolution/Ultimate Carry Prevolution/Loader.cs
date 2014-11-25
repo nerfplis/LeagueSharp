544ed3dbf641f59c3d56c5f17f7c1be3d716fe75
nerfplis <ryzoom3@hotmail.de>
2014-11-25 07:36:07 +0100

    First Commit

---
+++ Ultimate-Carry-Prevolution/Ultimate Carry Prevolution/Loader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using LeagueSharp;
+
+namespace Ultimate_Carry_Prevolution
+{
+	class Loader
+	{
+		public const string VersionNumber = "1.4";
+
+		public Loader()
+		{
+			Chat.WellCome();
+		}
+
+	
+	}
+}
